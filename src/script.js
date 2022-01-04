19d57f25b33fdd60c01406d1a790fecd80a1d301
MacDada <contact@dnowak.pl>
2022-01-04 12:37:25 +0100

    extracted init.js

--- src/script.js
+++ src/script.js
@@ -1,31 +1,5 @@
 'use strict';
 
-$('#content').prepend((function () {
-    const isDisabled = localStorage['dnthDisabled'];
-
-    const $button = $('<a href="#"></a>')
-        .text(isDisabled ? 'Włącz DnTpbHider' : 'Wyłącz DnTpbHider')
-        .click(function () {
-            if (isDisabled) {
-                localStorage.removeItem('dnthDisabled');
-            } else {
-                localStorage['dnthDisabled'] = 'true';
-            }
-
-            window.location.reload();
-        });
-
-    if (!isDisabled) {
-        runTpbHider({
-            hiddenOpacity: 0.2,
-            visibleFirst: true,
-            gcDays: 30
-        });
-    }
-
-    return $button;
-})());
-
 function runTpbHider(options) {
     const hiddenClass = 'dnthHidden';
 
